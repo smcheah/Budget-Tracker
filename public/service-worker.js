1aa6ee30e25b791ebef243cfa24bc187d8554541
Simone Cheah <simone_cheah@hotmail.com>
2020-12-15 05:57:09 +1100

    fix for save to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -42,26 +42,28 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     // non GET requests are not cached and requests to other origins are not cached
-    if (event.request.method !== "GET" || !event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(fetch(event.request));
-        return;
-    }
+    // if (event.request.method !== "GET" || !event.request.url.startsWith(self.location.origin)) {
+    //     console.log("one")
+    //     event.respondWith(fetch(event.request));
+    //     return;
+    // }
 
     // handle runtime GET requests for data from /api routes
-    if (event.request.url.includes("/api/transaction")) {
-        // make network request and fallback to cache if network request fails (offline)
-        event.respondWith(
-            caches.open(RUNTIME).then(cache => {
-                return fetch(event.request)
-                    .then(response => {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    })
-                    .catch(() => caches.match(event.request));
-            })
-        );
-        return;
-    }
+    // if (event.request.url.includes("/api/transaction")) {
+    //     // make network request and fallback to cache if network request fails (offline)
+    //     console.log("two")
+    //     event.respondWith(
+    //         caches.open(RUNTIME).then(cache => {
+    //             return fetch(event.request)
+    //                 .then(response => {
+    //                     cache.put(event.request, response.clone());
+    //                     return response;
+    //                 })
+    //                 .catch(() => caches.match(event.request));
+    //         })
+    //     );
+    //     return;
+    // }
 
     event.respondWith(caches
         .match(event.request)
@@ -70,11 +72,12 @@ self.addEventListener("fetch", event => {
             if (data) return data;
 
             return caches.open(RUNTIME).then(cache => {
+                console.log("three")
                 return fetch(event.request).then(response => {
-                    return cache
-                        .put(event.request, response.clone())
-                        .then(() => { return response; });
-                });
+                    cache.put(event.request, response.clone());
+                    return response;
+                })
+                    .catch(() => caches.match(event.request));
             });
         }));
 
