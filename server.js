7f2175fd71bc11f0544fa81da08a88066a212022
Simone Cheah <simone_cheah@hotmail.com>
2020-12-15 03:11:05 +1100

    add flags to mongoose connection

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ app.use( express.json() );
 app.use( express.static( "public" ) );
 
 mongoose.connect( process.env.MONGO_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
 } );
 
 // routes
 app.use( require( "./routes/api.js" ) );
 
 app.listen( PORT, () => {
-  console.log( `App running on port ${ PORT }!` );
+    console.log( `App running on http://localhost:${ PORT } !` );
 } );
