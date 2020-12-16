f8adf1eb7a4b0a4e88d696d56d141d52267ea618
Simone Cheah <simone_cheah@hotmail.com>
2020-12-17 09:20:33 +1100

    added bundled files

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ app.use(logger("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
     useNewUrlParser: true,
-    useFindAndModify: false,
     useUnifiedTopology: true
 });
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
 // routes
 app.use(require("./routes/api.js"));
