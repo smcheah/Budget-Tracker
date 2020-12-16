e28065728cc903775b2b8cff91188b7beead1702
Simone Cheah <simone_cheah@hotmail.com>
2020-12-17 09:38:01 +1100

    mode development

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const config = {
         path: `${__dirname}/public/dist`,
         filename: "[name].bundle.js"
     },
-    mode: "production",
+    mode: "development",
     module: {
         rules: [{
             test: /\.m?js$/,
