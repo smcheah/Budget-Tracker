ab5db35147ff9c34336c2cf884299ca6a016c03e
Simone Cheah <simone_cheah@hotmail.com>
2020-12-18 03:03:58 +1100

    update files

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(compression());
+
+// routes
+app.use(require("./routes/api.js"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/transaction", {
     useNewUrlParser: true,
@@ -18,9 +22,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/transaction", {
     useUnifiedTopology: true
 });
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-
-// routes
-app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT} !`);
